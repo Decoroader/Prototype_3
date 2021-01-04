9e75198b0e5d17d1b5b66b06acad26731a45f81b
Decoroader <secbittree@gmail.com>
2021-01-04 22:29:43 +0200

    Lesson 3.1 - Jump Force, 6, 7
    6.Tweak the jump force and gravity, 7.Prevent player from double-jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,29 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
+
     private Rigidbody playerRb;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         //playerRb.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
     }
+	private void OnCollisionEnter(Collision collision)
+	{
+        isOnGround = true;
+	}
 }
