9cd728c339f3907557c2b6ba8eb12234aa1298a8
Decoroader <secbittree@gmail.com>
2021-01-04 21:42:58 +0200

    Lesson 3.1 - Jump Force, 4, 5
    4.Make player jump at start, 5.Make player jump if spacebar pressed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        //playerRb.AddForce(Vector3.up * 1000);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
     }
 }
