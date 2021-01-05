7e0342365e760cdab7e7f1cdc750c36902c24b41
Decoroader <secbittree@gmail.com>
2021-01-05 21:49:48 +0200

    Lesson 3.3 - Don't Just Stand There, 7
    added flag for ban jump when character is unconscious, not good code w many flags

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
